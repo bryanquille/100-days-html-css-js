fb078f1333465a4041dc3192cbcca3e88adb9f66
Bryan Quille <103023942+bryanquille@users.noreply.github.com>
2024-11-18 15:33:12 -0500

    drum kits - fixing sound files

--- day-41/js/script.js
+++ day-41/js/script.js
@@ -1,7 +1,7 @@
-const crashAudio = new Audio('../assets/sounds/crash.mp3');
-const kickAudio = new Audio('../assets/sounds/kick.mp3');
-const snareAudio = new Audio('../assets/sounds/snare.mp3');
-const tomAudio = new Audio('../assets/sounds/tom.mp3');
+const crashAudio = new Audio('https://cdn.pixabay.com/audio/2024/09/16/audio_5823237b7f.mp3');
+const kickAudio = new Audio('https://cdn.pixabay.com/audio/2024/11/02/audio_b898b625db.mp3');
+const snareAudio = new Audio('https://cdn.pixabay.com/audio/2022/03/15/audio_cf8c31e615.mp3');
+const tomAudio = new Audio('https://cdn.pixabay.com/audio/2022/03/25/audio_e1ca492b34.mp3');
 const crashBtn = document.querySelector('#crash-btn');
 const kickBtn = document.querySelector('#kick-btn');
 const snareBtn = document.querySelector('#snare-btn');
